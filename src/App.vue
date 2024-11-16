1d2c761d4afb15b87bbdd81d13cbd6aa2c94b2f9
Safri Jawad <safrijawad1245@gmail.com>
2024-11-16 21:11:06 +0700

    feat: wip job listing

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
 <template>
-	<h1 class="text-4xl">Vue Jobs</h1>
+	<Navbar />
+	<!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
+	<Hero />
+	<HomeCards />
+	<JobListings />
 </template>

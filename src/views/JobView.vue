96993af653a61bab6bf07e868b95c4099eddd3e9
Safri Jawad <safrijawad1245@gmail.com>
2024-11-17 18:16:51 +0700

    feat: crud jobs

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,11 +1,14 @@
 <script setup>
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import { reactive, onMounted } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute, RouterLink, useRouter } from "vue-router";
 import BackButton from "@/components/BackButton.vue";
 import axios from "axios";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
 
 const jobId = route.params.id;
 
@@ -13,6 +16,19 @@ const state = reactive({
 	job: {},
 	isLoading: true,
 });
+
+const deleteJob = async () => {
+	try {
+		const confirm = window.confirm("Are you sure you want to delete this job?");
+		if (!confirm) return;
+		await axios.delete(`/api/jobs/${jobId}`);
+		toast.success("Job deleted successfully");
+		router.push("/jobs");
+	} catch (error) {
+		console.log("Error deleting job", error);
+		toast.error("Error deleting job");
+	}
+};
 
 onMounted(async () => {
 	try {
@@ -40,8 +56,7 @@ onMounted(async () => {
 						<h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
 						<div
 							class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-							<i
-								class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+							<i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
 							<p class="text-orange-700">{{ state.job.location }}</p>
 						</div>
 					</div>
@@ -97,6 +112,7 @@ onMounted(async () => {
 							>Edit Job</RouterLink
 						>
 						<button
+							@click="deleteJob"
 							class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
 							Delete Job
 						</button>
